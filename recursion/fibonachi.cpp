9b94f089002debaa76e2a55df9d18beedc3fa729
hamzargc <34453653+hamzargc@users.noreply.github.com>
2020-01-03 20:47:22 +0500

    Add files via upload

---
+++ recursion/fibonachi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int num);
+int main()
+{
+	int num;
+	cout << "Enter factiorial";
+	cin >> num;
+	cout << fibo(num);
+	
+}
+
+fibo(int num)
+{
+	if (num==0) return 0;
+	if (num==1) return 1;
+	return fibo(num-1) + fibo(num-2);
+}
